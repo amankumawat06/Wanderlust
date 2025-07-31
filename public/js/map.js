83fc4fc268e536e7ec3d8ac44698f44228768727
Aman kumawat <amankumawat4507@gmail.com>
2025-07-31 07:10:45 -0700

    Add new lines

--- public/js/map.js
+++ public/js/map.js
@@ -1,27 +1,8 @@
-// const map = L.map("map").setView([coordinates[1], coordinates[0]], 13);
+const map = L.map("map").setView([coordinates[1], coordinates[0]], 13);
 
-// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-// }).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+}).addTo(map);
 
-// L.marker([coordinates[1], coordinates[0]])
-//   .addTo(map)
-//   .openPopup();
-
-
-
-fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${listing.location}`, {
-  headers: {
-    "User-Agent": "YourAppName/1.0 (your@email.com)"  // Required by Nominatim API
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    const coordinates = [data[0].lon, data[0].lat];
-    const map = L.map("map").setView([coordinates[1], coordinates[0]], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-    L.marker([coordinates[1], coordinates[0]]).addTo(map).openPopup();
-  })
-  .catch(err => {
-    console.error("Error fetching coordinates:", err);
-  });
+L.marker([coordinates[1], coordinates[0]])
+  .addTo(map)
+  .openPopup();
